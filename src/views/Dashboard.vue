a79a9ab5f11dad903545089d6d33c4e8492ecb92
gaylonalfano <gaylon.alfano@gmail.com>
2021-01-22 10:29:05 +0800

    Updated Dashboard to use the new data model structure with a nested ARRAY of MAPS inside the memmber doc to represent the ACCOUNTS. Nice an easy honestly

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,18 @@
 <template>
   <Navbar />
-  <div v-if="memberOneAccounts">
-    <h1>memberOneAccounts:</h1>
-    {{ memberOneAccounts }}
-  </div>
-  <div v-if="memberTwoAccounts">
-    <h1>memberTwoAccounts:</h1>
-    {{ memberTwoAccounts }}
-  </div>
-  <div v-if="membersError">members error: {{ membersError }}</div>
-  <div v-if="memberOneAccountsError">
-    memberOneAccountsError: {{ memberOneAccountsError }}
-  </div>
-  <div v-if="memberTwoAccountsError">
-    memberTwoAccountsError: {{ memberTwoAccountsError }}
+  <div v-if="members">
+    <div v-for="member in members" :key="member.id">
+      <h2>name: {{ member.name }}</h2>
+      <h2>tracker: {{ member.trackerOwner }}</h2>
+      <div v-for="account in member.accounts" :key="account.type">
+        <h3>type: {{ account.type }}</h3>
+        <p>balance: {{ account.balance }}</p>
+        <p>
+          previousTransactionAmount: {{ account.previousTransactionAmount }}
+        </p>
+        <p>previousTransactionDate: {{ account.previousTransactionDate }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,38 +32,45 @@ export default defineComponent({
   name: "Dashboard",
   components: { Navbar },
   setup() {
-    // This is a general generic all members collection grab
-    const { error: membersError, documents: members } = getCollection(
-      "members"
-    );
+    const { error, documents: members } = getCollection("members-simple");
 
-    // This grabs a SINGLE member's accounts subcollection and orderBy("createdAt") (by default)
-    // NOTE This does NOT require a composite index (unlike below)
-    const {
-      error: memberTwoAccountsError,
-      documents: memberTwoAccounts,
-    } = getCollection("members/S1z2W4pFlzf6CXKwk2Vb/accounts");
-
-    // This is a query using where() on a SINGLE member accounts subcollection
-    // NOTE This requires a COMPOSITE INDEX to be created
-    const {
-      error: memberOneAccountsError,
-      documents: memberOneAccounts,
-    } = getCollection("members/fwy89arzwfXABvYEbdCo/accounts", [
-      "type",
-      "==",
-      "savings",
-    ]);
-
-    return {
-      membersError,
-      members,
-      memberOneAccountsError,
-      memberOneAccounts,
-      memberTwoAccountsError,
-      memberTwoAccounts,
-    };
+    return { error, members };
   },
+
+  // ===== OLD Data Model
+  // setup() {
+  //   // This is a general generic all members collection grab
+  //   const { error: membersError, documents: members } = getCollection(
+  //     "members"
+  //   );
+
+  //   // This grabs a SINGLE member's accounts subcollection and orderBy("createdAt") (by default)
+  //   // NOTE This does NOT require a composite index (unlike below)
+  //   const {
+  //     error: memberTwoAccountsError,
+  //     documents: memberTwoAccounts,
+  //   } = getCollection("members/S1z2W4pFlzf6CXKwk2Vb/accounts");
+
+  //   // This is a query using where() on a SINGLE member accounts subcollection
+  //   // NOTE This requires a COMPOSITE INDEX to be created
+  //   const {
+  //     error: memberOneAccountsError,
+  //     documents: memberOneAccounts,
+  //   } = getCollection("members/fwy89arzwfXABvYEbdCo/accounts", [
+  //     "type",
+  //     "==",
+  //     "savings",
+  //   ]);
+
+  //   return {
+  //     membersError,
+  //     members,
+  //     memberOneAccountsError,
+  //     memberOneAccounts,
+  //     memberTwoAccountsError,
+  //     memberTwoAccounts,
+  //   };
+  // },
 });
 </script>
 
