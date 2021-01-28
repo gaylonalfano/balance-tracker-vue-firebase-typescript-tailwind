c24791c333bf762f0e64278b354ef47625f65a52
gaylonalfano <gaylon.alfano@gmail.com>
2021-01-28 09:26:11 +0800

    Added dynamic arrows for latestTransactionAmount on the stats cards components.

--- src/components/AccountDetailsStatsCard.vue
+++ src/components/AccountDetailsStatsCard.vue
@@ -13,7 +13,9 @@
           class="flex items-baseline mt-1 text-3xl font-semibold text-gray-900"
         >
           ¥{{ account.balance }}
+          <!-- Latest Transaction Arrow Indicator -->
           <div
+            v-if="account.latestTransactionAmount > 0"
             class="ml-2 flex items-baseline text-sm font-semibold text-green-600"
           >
             <svg
@@ -29,6 +31,25 @@
               />
             </svg>
             <span class="sr-only"> Increased by </span>
+            {{ account.latestTransactionAmount }}
+          </div>
+          <div
+            v-else
+            class="ml-2 flex items-baseline text-sm font-semibold text-red-600"
+          >
+            <svg
+              class="self-center flex-shrink-0 h-5 w-5 text-red-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <span class="sr-only"> Decreased by </span>
             {{ account.latestTransactionAmount }}
           </div>
           <!-- Add Transaction Router Link -->
